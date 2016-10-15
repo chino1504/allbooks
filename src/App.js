74c57180b66e814f513398cb455040583c7209a4
chino1504 <c.c.sanchez@gmail.com>
2016-10-14 21:54:47 -0300

    fixin identation

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import AppInput from './form-components/app-input';
-import { Form, Jumbotron, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Form inline>
-          <AppInput inputLabel="Nombre" placeholder="Nombre"/>
-          <AppInput inputLabel="Autor" placeholder="Autor"/>
-          <AppInput inputLabel="Año" placeholder="Año"/>
-          <AppInput inputLabel="Descripción" placeholder="Descripción"/>
-        </Form>
-        <p className="App-intro">
-          To get started, ask help to rodri.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div className="App">
+            <Header />
+            <Form inline>
+              <AppInput inputLabel="Nombre" placeholder="Nombre"/>
+              <AppInput inputLabel="Autor" placeholder="Autor"/>
+              <AppInput inputLabel="Año" placeholder="Año"/>
+              <AppInput inputLabel="Descripción" placeholder="Descripción"/>
+            </Form>
+            <p className="App-intro">
+              To get started, ask help to rodri.
+            </p>
+          </div>
+        );
+    }
 }
 
 export default App;
--- src/form-components/app-input.js
+++ src/form-components/app-input.js
@@ -4,23 +4,23 @@ import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 class AppInput extends Component {
 
-	constructor (props) {
-		super(props);
-	}
+    constructor (props) {
+        super(props);
+    }
 
-	render() {
-	return (
-		<FormGroup controlId="formInlineName" className="app-input">
-	  		<ControlLabel>{this.props.inputLabel}</ControlLabel>
-	  		<FormControl type="text" placeholder={this.props.placeholder} />
-		</FormGroup>
-	);
-	}
+    render() {
+        return (
+            <FormGroup controlId="formInlineName" className="app-input">
+                <ControlLabel>{this.props.inputLabel}</ControlLabel>
+                <FormControl type="text" placeholder={this.props.placeholder} />
+            </FormGroup>
+        );
+    }
 }
 
 AppInput.propTypes = {
-	inputLabel: React.PropTypes.string,
-  	placeholder: React.PropTypes.string
+    inputLabel: React.PropTypes.string,
+    placeholder: React.PropTypes.string
 };
 
 export default AppInput;
