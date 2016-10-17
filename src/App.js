8a265f4112412cc1fdd05303bc1fb876dc4c66e0
chino1504 <c.c.sanchez@gmail.com>
2016-10-16 23:16:28 -0300

    create first form component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
-import AppInput from './form-components/app-input';
+import FormSearch from './form-components/form-search';
 import { Form } from 'react-bootstrap';
 
 class App extends Component {
     render() {
         return (
-          <div className="App">
-            <Header />
-            <Form inline>
-              <AppInput inputLabel="Nombre" placeholder="Nombre"/>
-              <AppInput inputLabel="Autor" placeholder="Autor"/>
-              <AppInput inputLabel="Año" placeholder="Año"/>
-              <AppInput inputLabel="Descripción" placeholder="Descripción"/>
-            </Form>
-            <p className="App-intro">
-              To get started, ask help to rodri.
-            </p>
-          </div>
+            <div className="App">
+                <Header />
+                <Form>
+                    <FormSearch />
+                </Form>
+            </div>
         );
     }
 }
